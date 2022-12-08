b58f0f665938ae3ef8954c3d2c47c14be7dfe8e2
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2022-12-09 01:13:40 +0530

    interface

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,9 +1,16 @@
 // index.js
 import type { NextPage } from 'next'
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import sanityClient from "../sanity"
 
+import { Post } from "../typings"
 
-const Home : NextPage = () => {
+interface Props {
+  posts: Post[]
+}
+const Home : NextPage = ({posts}: Props) => {
+
   return (
     <div>
     <Head>
@@ -17,3 +24,28 @@ const Home : NextPage = () => {
 }
 
 export default Home
+
+
+
+export const getServerSideProps: GetServerSideProps = async() =>  {
+  const query = `*[_type == "post"] {
+    _id,
+    title,
+    slug,
+    author -> {
+      name,
+      image
+    },
+      description,
+      mainImage,
+      slug
+  }
+  `
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
---
+++ web/typings.d.ts
@@ -0,0 +1,21 @@
+export interface Post {
+    _id:string;
+    _createdAt: string;
+    title:string;
+    slug:{
+      current: string
+    };
+    author:  {
+      name:string;
+      image:string;
+    },
+    description:string;
+    mainImage:  {
+      asset: {
+        url:string;
+      };
+    };
+    slug:string;
+    body: [object];
+  
+}
